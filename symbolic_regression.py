325c9d57d97f1f8f0fb296f6c98866b6065ebf54
simonhwk <hk3234@columbia.edu>
2023-09-05 20:17:48 -0400

    update symbolic_regression.py

--- symbolic_regression.py
+++ symbolic_regression.py
@@ -1,22 +1,2 @@
 
-# How to plot a .txt file in Matplotlib
-
-# Plotting with Matplot lib
-
-
-import matplotlib.pyplot as plt
-
-x = []
-y = []
-
-for line in open('data2022_Gold.txt', 'r'):
-    lines = [i for i in line.split(", ")]
-    x.append(float(lines[0]))
-    y.append(float(lines[1]))
-
-plt.title("Symbolic Regression (RS)")
-plt.xlabel("x")
-plt.ylabel("y")
-plt.plot(x,y, marker = 'o', c='g')
-
-plt.show()
+# Symbolic Regression using GA
